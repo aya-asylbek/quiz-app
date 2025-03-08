e4b086b1d9f0d5c952a6ee0eda380b988aa1f108
aya-asylbek <aichurek0990@gmail.com>
2025-03-07 21:38:53 -0800

    Add ES Module fake data

---
+++ server/fakedata.js
@@ -0,0 +1,128 @@
+export default [
+    {
+        "response_code": 0,
+        "results": [
+          {
+            "type": "multiple",
+            "difficulty": "medium",
+            "category": "Science: Computers",
+            "question": "In programming, the ternary operator is mostly defined with what symbol(s)?",
+            "correct_answer": "?:",
+            "incorrect_answers": [
+              "??",
+              "if then",
+              "?"
+            ]
+          },
+          {
+            "type": "multiple",
+            "difficulty": "medium",
+            "category": "Science: Computers",
+            "question": ".rs is the top-level domain for what country?",
+            "correct_answer": "Serbia",
+            "incorrect_answers": [
+              "Romania",
+              "Russia",
+              "Rwanda"
+            ]
+          },
+          {
+            "type": "multiple",
+            "difficulty": "medium",
+            "category": "Science: Computers",
+            "question": "What was the first commerically available computer processor?",
+            "correct_answer": "Intel 4004",
+            "incorrect_answers": [
+              "Intel 486SX",
+              "TMS 1000",
+              "AMD AM386"
+            ]
+          },
+          {
+            "type": "multiple",
+            "difficulty": "medium",
+            "category": "Science: Computers",
+            "question": "Which one of these is not an official development name for a Ubuntu release?",
+            "correct_answer": "Mystic Mansion",
+            "incorrect_answers": [
+              "Trusty Tahr",
+              "Utopic Unicorn",
+              "Wily Werewolf"
+            ]
+          },
+          {
+            "type": "multiple",
+            "difficulty": "medium",
+            "category": "Science: Computers",
+            "question": "When was the programming language &quot;C#&quot; released?",
+            "correct_answer": "2000",
+            "incorrect_answers": [
+              "1998",
+              "1999",
+              "2001"
+            ]
+          },
+          {
+            "type": "multiple",
+            "difficulty": "medium",
+            "category": "Science: Computers",
+            "question": "What is the number of keys on a standard Windows Keyboard?",
+            "correct_answer": "104",
+            "incorrect_answers": [
+              "64",
+              "94",
+              "76"
+            ]
+          },
+          {
+            "type": "multiple",
+            "difficulty": "medium",
+            "category": "Science: Computers",
+            "question": "How many bits make up the significand portion of a single precision floating point number?",
+            "correct_answer": "23",
+            "incorrect_answers": [
+              "8",
+              "53",
+              "15"
+            ]
+          },
+          {
+            "type": "multiple",
+            "difficulty": "medium",
+            "category": "Science: Computers",
+            "question": "What is the name of the default theme that is installed with Windows XP?",
+            "correct_answer": "Luna",
+            "incorrect_answers": [
+              "Neptune",
+              "Whistler",
+              "Bliss"
+            ]
+          },
+          {
+            "type": "multiple",
+            "difficulty": "medium",
+            "category": "Science: Computers",
+            "question": "Generally, which component of a computer draws the most power?",
+            "correct_answer": "Video Card",
+            "incorrect_answers": [
+              "Hard Drive",
+              "Processor",
+              "Power Supply"
+            ]
+          },
+          {
+            "type": "multiple",
+            "difficulty": "medium",
+            "category": "Science: Computers",
+            "question": "What does the &#039;S&#039; in the RSA encryption algorithm stand for?",
+            "correct_answer": "Shamir",
+            "incorrect_answers": [
+              "Secure",
+              "Schottky",
+              "Stable"
+            ]
+          }
+        ]
+      }
+    ];
+    
